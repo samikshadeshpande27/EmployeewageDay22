7c347e95ebc61f220664f5489ada5f5d4ae05ab8
samiksha <samikshadeshpande27@gmail.com>
2020-05-19 00:04:02 -0700

    UC1 is added

--- EmployeewageDay22.java
+++ EmployeewageDay22.java
@@ -1,1 +1,13 @@
-System.out.println("Welcome to Employee wage computation");
+public class EmployeeWageUC1{
+        public static void main(String[] args)
+        {
+        int IS_FULL_TIME=1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == IS_FULL_TIME)
+                System.out.println("Employee is present");
+        else
+                System.out.println("Employee is absent");
+
+        }
+}
+
