6650e177c1eb337cad35a40c6aeb95a82f4a0058
samiksha <samikshadeshpande27@gmail.com>
2020-05-19 00:06:06 -0700

    UC2 is added

--- EmployeewageDay22.java
+++ EmployeewageDay22.java
@@ -1,13 +1,21 @@
-public class EmployeeWageUC1{
+public class EmployeewageUC2
+{
         public static void main(String[] args)
         {
-        int IS_FULL_TIME=1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == IS_FULL_TIME)
-                System.out.println("Employee is present");
-        else
-                System.out.println("Employee is absent");
+                int IS_FULL_TIME = 1;
+                int EMP_RATE_PER_HOUR = 20;
+                int empHrs = 0;
+                int empWage = 0;
+                double empCheck = Math.floor(Math.random() * 10) % 2;
+                if (empCheck == IS_FULL_TIME)
+                        empHrs=8;
+                else
+                         empHrs=0;
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                System.out.println("Emp Wage :" +empWage);
 
         }
+
 }
+
 
