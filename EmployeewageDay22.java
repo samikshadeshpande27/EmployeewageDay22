02ee2640cd1472f99f997fc9558ad7baa7896a0c
samiksha <samikshadeshpande27@gmail.com>
2020-05-19 00:12:50 -0700

    UC5 is added

--- EmployeewageDay22.java
+++ EmployeewageDay22.java
@@ -1,14 +1,19 @@
-public class EmployeewageUC4
+public class EmployeeWageUC5
 {
         public static final int IS_PART_TIME = 1;
         public static final int IS_FULL_TIME = 2;
         public static final int EMP_RATE_PER_HOUR = 20;
+        public static final int NUM_OF_WORKING_DAYS = 2;
         public static void main(String[] args)
         {
                 int empHrs = 0;
                 int empWage = 0;
+                int totalEmpWage = 0;
+                for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+                {
                 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-                switch (empCheck) {
+                switch (empCheck)
+                {
                         case IS_PART_TIME:
                                 empHrs = 4;
                                 break;
@@ -19,8 +24,11 @@ public class EmployeewageUC4
                                         empHrs = 0;
                 }
                 empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
                 System.out.println("Emp Wage:" + empWage);
         }
+   System.out.println("Total Emp Wage:"+ totalEmpWage);
+}
 
 }
 
